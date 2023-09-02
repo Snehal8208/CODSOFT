91c7cb9ea9f28a23bae1e65abeb71c7e2ed83fc1
Snehal8208 <142528258+Snehal8208@users.noreply.github.com>
2023-09-02 21:01:49 +0530

    Add files via upload

---
+++ NumberGame.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        int minRange = 1;
+        int maxRange = 100;
+        int maxAttempts = 10;
+        
+        int score = 0;
+        boolean playAgain = true;
+        
+        System.out.println("Welcome to the Number Guessing Game!");
+        
+        while (playAgain) {
+            int targetNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            int attempts = 0;
+            
+            System.out.println("I've selected a number between " + minRange + " and " + maxRange + ". Try to guess it!");
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == targetNumber) {
+                    System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+                    score += (maxAttempts - attempts + 1);
+                    break;
+                } else if (userGuess < targetNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (attempts >= maxAttempts) {
+                System.out.println("Sorry, you've used all your attempts. The correct number was " + targetNumber);
+            }
+
+            System.out.print("Your current score is: " + score + ". Play again? (yes/no): ");
+            String playAgainInput = scanner.next().toLowerCase();
+            playAgain = playAgainInput.equals("yes");
+        }
+
+        System.out.println("Thanks for playing! Your final score is: " + score);
+        scanner.close();
+    }
+}
